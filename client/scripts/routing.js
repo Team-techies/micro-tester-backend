e43acea2959767abac8da085c282260793fd6501
Prathu Mandawariya <pmandawa@igategegdc.com>
2017-06-20 17:32:30 +0530

    Splitted Controller

--- client/scripts/routing.js
+++ client/scripts/routing.js
@@ -1,7 +1,7 @@
 var app = angular.module('microTester');
 
 app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-    /*  $urlRouterProvider.otherwise("/overview");*/
+    $urlRouterProvider.otherwise("/overview");
 
     $stateProvider
         .state('overview', {
@@ -19,23 +19,54 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
         .state('testClient', {
             url: "/testClient",
             templateUrl: "../views/tester.html",
-            params:{
-                suite:{},
-                requests:[]
+            params: {
+                suite: {},
+                requests: []
             }
         })
+        .state('testClient.testerHeader', {//nested state [products is the nested state of business state]
+            url: "/testerHeader",
+            templateUrl: "../views/testerHeader.html"
+
+        })
+        .state('testClient.testerBody', {//nested state [products is the nested state of business state]
+            url: "/testerBody",
+            templateUrl: "../views/testerBody.html"
+
+        })
+
+
+
         .state('appSettings', {
             url: "/appSettings",
             templateUrl: "../views/appSettings.html"
         })
         .state('applicationSettings', {
             url: "/applicationSettings",
+           
             templateUrl: "../views/applicationSettings.html"
         })
         .state('testSuiteSettings', {
             url: "/testSuiteSettings",
+            
+            controller: "SettingsController",
             templateUrl: "../views/testSuiteSettings.html"
         })
+        
+        .state('testSuiteSettings.testAppsettings', {//nested state [products is the nested state of business state]
+            url: "/testAppsettings",
+            templateUrl: "../views/testSuite_app.html"
+
+        })
+
+          .state('testSuiteSettings.testTSuiteSettings', {//nested state [products is the nested state of business state]
+            url: "/testTSuiteSettings",
+            templateUrl: "../views/testSuite_tester.html"
+
+        })
+
+
+
         .state('existingTestSuite', {
             url: "/existingTestSuite",
             templateUrl: "../views/existingTestSuite.html"
--- client/scripts/settings.js
+++ client/scripts/settings.js
@@ -1,25 +1,149 @@
+var app = angular.module('microTester');
 
-// app.controller('settingsController', function ($scope) {
-//   $scope.testSuiteArray = ["a", "b", "c", "d", "e", "f"];
-//   $scope.applicationData = {};
-//   $scope.testSuiteData = {};
-//   $scope.showTestSuiteConfiguration = false;
+app.controller('SettingsController',['$state','$scope', '$http',     function($state,$scope, $http, $window){
 
-//   $scope.showConfiguration = function () {
-//     $scope.showTestSuiteConfiguration = true;
-//   }
+    $scope.data = false;
+    $scope.read = true;
+    $scope.appData={};
 
-//   $scope.hideConfiguration = function () {
-//     $scope.showTestSuiteConfiguration = false;
-//   }
+       function onload(){
+        $state.go('testSuiteSettings.testAppsettings');
+        /*alert();*/
+    }
+    onload();
 
-//   $scope.saveApplicationData = function () {
-//     console.log($scope.applicationData);
-//   }
+    $scope.getUser = function () {
+        console.log("hell0");
+        $http({
+            url: '/getApp',
+            method: "GET",
+            // data: $scope.user,
+            // headers: { 'Content-Type': 'application/json' }
+        }).then(function (response) {
+            console.log(response.data.stat);
+            if (response.data.stat) {
+                $scope.name = response.data.name;
+                $scope.email = response.data.email;
+                $scope.appData = response.data.doc;
+            } else if (response.data.msg === "please login to create app ") {
+                $window.location.href = '../views/index.html';
+            }
+            else {
+                alert(response.data.msg);
+            }
+        }, function (err) {
+            console.log(err);
+        });
+    };
 
-//   $scope.saveTestSuiteData = function () {
+    $scope.appConfig = function () {
+        if (!$scope.data) {
+            $scope.data = true;
+            $scope.read = false;
+        }
+        else {
+            $http({
+                url: '/configApp',
+                method: "POST",
+                headers: { 'ContentType': 'application/json' },
+                data: $scope.appData || {}
+            }).then(function (response) {
+                console.log(response.data.stat);
+                if (response.data.stat) {
+                    $scope.data = false;
+                    $scope.read = true;
+                } else if (response.data.msg === "please login to create app ") {
+                    $window.location.href = '../views/index.html';
+                }
+                else {
+                    alert(response.data.msg);
+                }
+            }, function (err) {
+                console.log(err);
+            });
+        }
 
-//     console.log($scope.testSuiteData);
-//   }
 
-// })
+    }
+
+
+}]);
+
+
+app.controller('TestSuiteSettingsController', ['$state','$scope', '$http',     function($state,$scope, $http, $window) {
+
+  $scope.suiteConfig = function () {
+        console.log($scope.testsuite);
+        if (!$scope.data) {
+            $scope.data = true;
+            $scope.read = false;
+        }
+        else {
+            $http({
+                url: '/configSuite',
+                method: "POST",
+                headers: { 'ContentType': 'application/json' },
+                data: $scope.testsuite || {}
+            }).then(function (response) {
+                console.log(response.data.stat);
+                if (response.data.stat) {
+                    $scope.data = false;
+                    $scope.read = true;
+                } else if (response.data.msg === "please login to create app ") {
+                    $window.location.href = '../views/index.html';
+                }
+                else {
+                    alert(response.data.msg);
+                }
+            }, function (err) {
+                console.log(err);
+            });
+        }
+
+
+    }
+
+
+$scope.showTestSuiteConfiguration = false;
+ 
+    $scope.showConfiguration = function () {
+
+        for (var i = 0; i < $scope.suites.length; i++) {
+            if ($scope.suites[i]._id === $scope.suite._id) {
+                $scope.testsuite = $scope.suites[i];
+            }
+            $scope.showTestSuiteConfiguration = true;
+            console.log($scope.testsuite);
+        }
+    }
+
+  $scope.getTestSuite = function () {
+
+        $http({
+            method: "GET",
+            url: "/getTestSuite",
+        }).then(function successCallback(response) {
+            if (response.data.stat) {
+                if (response.data.doc.length != 0) {
+                    $scope.suites = response.data.doc;
+                }
+                else {
+                    $scope.suites = undefined;
+                }
+
+            } else if (response.data.msg === "please login to create app ") {
+                $window.location.href = '../views/index.html';
+            }
+            else {
+                alert(response.data.msg);
+            }
+        }, function errorCallback(err) {
+            console.log(response.data.msg);
+            alert(response.data.msg);
+
+        });
+    };
+
+
+
+}]);
